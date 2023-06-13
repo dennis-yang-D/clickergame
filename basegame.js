026ea2372011ecf0ebcbb6eabf8c2d74febbbb63
dennis-yang-D <58886346+dennis-yang-D@users.noreply.github.com>
2023-06-12 22:19:22 -0400

    Update basegame.js

--- basegame.js
+++ basegame.js
@@ -2,9 +2,9 @@ var points = 0;
 var points per click = 0;
 
 document.getElementById("showPoints").innerHTML = points
-document.getElementById('pointCounter').addEventListener("click", baseClick)
+document.getElementById('clickingButton').addEventListener("click", baseClick)
 function baseClick(){
     points += 1
     console.log(points)
-    document.getElementById("counter").innerHTML = points
+    document.getElementById("showPoints").innerHTML = points
 }
