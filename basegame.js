e3d09d8a5968ba71dfeaaa31063a850c820ea093
dennis-yang-D <58886346+dennis-yang-D@users.noreply.github.com>
2023-06-12 23:23:14 -0400

    Update basegame.js, include an upgrade button

--- basegame.js
+++ basegame.js
@@ -1,10 +1,18 @@
 var points = 0;
 var pointsPerClick = 1;
+var clickUpgradeCostNum = 10;
 
 document.getElementById("showPoints").innerHTML = points
 document.getElementById('clickingButton').addEventListener("click", baseClick)
+document.getElementById("clickUpgradeCost").innerHTML = clickUpgradeCostNum
+document.getElementById('upgradeButton').addEventListener("click", upgradeClick)
+
 function baseClick(){
     points += pointsPerClick
-    console.log(points)
     document.getElementById("showPoints").innerHTML = points
+}
+function upgradeClick(){
+    clickUpgradeCostNum = clickUpgradeCostNum + 0.5 * clickUpgradeCostNum
+    pointsPerClick = pointsPerClick + 1
+    document.getElementById("clickUpgradeCost").innerHTML = clickUpgradeCostNum
 }
