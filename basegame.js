b2f6d662e3c831562b5c3bda063baee5261cf8ad
dennis-yang-D <58886346+dennis-yang-D@users.noreply.github.com>
2023-06-12 22:16:10 -0400

    Update basegame.js with a click function to increment the points

--- basegame.js
+++ basegame.js
@@ -1,2 +1,10 @@
 var points = 0;
 var points per click = 0;
+
+document.getElementById("showPoints").innerHTML = points
+document.getElementById('pointCounter').addEventListener("click", baseClick)
+function baseClick(){
+    points += 1
+    console.log(points)
+    document.getElementById("counter").innerHTML = points
+}
