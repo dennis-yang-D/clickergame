1fc2f1bb15b7e948dd1aed793c5c224a7771cd20
dennis-yang-D <58886346+dennis-yang-D@users.noreply.github.com>
2023-06-25 20:08:10 -0400

    Create savegame.js
    Made save file

---
+++ savegame.js
@@ -0,0 +1,7 @@
+import * as baseVars from './basegame.js'
+
+document.getElementById('saveButton').addEventListener("click", exportSave)
+
+function exportSave(){
+  document.getElementById('exportSaveSpace').innerHTML = String(baseVars.points) + "A" + String(baseVars.pointsPerClick) "A" + String(baseVars.totalPointsPerClick) + "A" + baseVars.clickUpgradeCosts.toString() + "A" + baseVars.clickUpgradeBoosts.toString() + "A" + baseVars.clickUpgradeCostInc.toString() + "A" + String(baseVars.prestigeBonus1) + "A" + String(baseVars.prestigeMachines) + "A" + String(baseVars.prestigeBonusCost1)
+}
